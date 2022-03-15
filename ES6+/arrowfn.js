740fe0def9b484e0a555d70fa63de75a1d48e662
Raj Thombare (Rakaa) <therajthombare@gmail.com>
2022-03-16 01:37:51 +0530

    done: includes & default parameters

---
+++ ES6+/arrowfn.js
@@ -0,0 +1,36 @@
+/* Arrow Functions (=>) 
+they have a nice, shorter syntax
+they solve the 'this' binding 
+
+function foo(a, b){
+    console.log(a + b)
+}
+
+foo(6, 5);
+
+// instead
+
+var x = (a, b) => a + b;
+var result = x(6,5);
+console.log(result) */
+
+
+/* Default Parameters */
+
+/*
+let x = (y = 10, z = 20) => {
+    let a = y 
+    let b = z 
+    console.log(a + b)
+}
+
+x();
+*/
+
+/* Includes */
+
+var myArray = [1, 2, 3, 4, 5];
+console.log(myArray.includes(5)); // true
+console.log(myArray.includes(11)); // false
+
+
