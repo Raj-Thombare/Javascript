68d688e0f78ac6eee39d6fa8bf04a5cf996c6f60
Raj Thombare (Rakaa) <therajthombare@gmail.com>
2022-03-23 01:23:31 +0530

    Done with Sets

--- ES6+/arraymethods.js
+++ ES6+/arraymethods.js
@@ -1,9 +1,57 @@
+/* Set & Map */
+
+// Set --> It return an Object with unique values!
+
+const uniqueNumbers = new Set();
+
+uniqueNumbers.add(31)
+uniqueNumbers.add(11)
+uniqueNumbers.add(11)
+uniqueNumbers.add(45)
+
+console.log(uniqueNumbers) // {31, 11, 45}
+
+console.log(uniqueNumbers.has(45)) // true
+console.log(uniqueNumbers.has(12)) // false
+
+const numbers = [10, 50, 30, 40, 50]
+const uniqueValues = new Set(numbers)
+console.log(uniqueValues) // {10, 50, 30, 40}
+
+console.log(Array.from(uniqueValues)) // [10, 50, 30, 40]
+
+
+// Map --> It return an Object with unique values!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 Array Methods --> Find & FindIndex 
 These both methods return an object!
 */
 
-
+/*
 // Find
 const players = [{name:'Rakaa'}, {name:'Raj'}, {name:'Rohit'}]
 
@@ -29,3 +77,4 @@ const bowler = bowlers.findIndex(bowler => {
     return bowler === 'Hazlewood'
 })
 console.log(bowler) // 0
+*/
